2482ea617ee7afd0cc30e8fb7335c16817b8f2d1
prashantm-exalogic <prashant@exalogic.in>
2018-08-27 10:32:13 +0530

    initial commit

---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Main.css";
+import Empty from "./Empty";
+import ChatWindow from "./ChatWIndow";
+const Main = ({ user, activeUserId }) => {
+const renderMainContent = () => {
+if (!activeUserId) {
+return <Empty user={user} activeUserId={activeUserId} />;
+} else {
+return <ChatWindow activeUserId={activeUserId} />;
+}
+};
+return <main className="Main">{renderMainContent()}</main>;
+};
+export default Main;
